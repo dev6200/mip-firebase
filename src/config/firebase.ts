98441f0f8ce42f82c13f882b3fdcea73a742d406
kendrickeong <94017998+kendrickeong@users.noreply.github.com>
2025-01-26 19:54:49 +0800

    Add note to move the configs to the env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,6 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Move this to env
 const firebaseConfig = {
   apiKey: "AIzaSyCWBd2LDl1uourewe5xnCoUFgMMxvsaP1Q",
   authDomain: "mip-song-collection.firebaseapp.com",
